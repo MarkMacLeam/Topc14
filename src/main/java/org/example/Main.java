4dac10e64f5d327818e4a6d5702eff55d2e1a44b
macleam <markmaclean1406@gmail.com>
2022-11-10 11:01:44 +0000

    Alf Stewart

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 //comment
-        System.out.println("G'Day!");
+        System.out.println("G'Day! Alf Stewart Is the Man");
     }
 }
