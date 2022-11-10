33f4db999b3d25a19a946e3110d80d41783916b8
MarkMacLeam <117924500+MarkMacLeam@users.noreply.github.com>
2022-11-10 14:59:00 +0000

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
 //comment
         System.out.println("G'Day! Alf Stewart Is the Man");
+        System.out.println("Harold Bishop would smash him");
+        
     }
 }
